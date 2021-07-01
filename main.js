08ed7e83d9fe84158420572ef9e2fe45b4c64435
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-07-01 08:35:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,24 @@ let paper_card = player_card[1];
 let scissors_card = player_card[2];
 let card = "No card";
 let bot_card = "No card";
-
+// console.log(rock_card, paper_card, scissors_card);
+const bot_play = () => {
+  let bot_card_int = parseInt((Math.random() * (3 - 1 + 1)), 10) + 1;
+  switch (bot_card_int) {
+    case 1:
+    bot_card = "r"
+      break;
+    case 2:
+    bot_card = "p"
+      break;
+    case 3:
+    bot_card = "s"
+      break;
+    default:
+      "No card"
+  }
+  return bot_card;
+}
 rock_card.onclick = async () => {
   console.log("rock");
   card = "r";
